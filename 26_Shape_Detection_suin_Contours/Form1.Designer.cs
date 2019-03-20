29e486297cc8a9338fb34f68c2532892e4c58162
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-03-20 13:54:37 +0100

    Shape Detection

---
+++ 26_Shape_Detection_suin_Contours/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace _26_Shape_Detection_using_Contours
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shapeDetectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.shapeDetectionToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // shapeDetectionToolStripMenuItem
+            // 
+            this.shapeDetectionToolStripMenuItem.Name = "shapeDetectionToolStripMenuItem";
+            this.shapeDetectionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shapeDetectionToolStripMenuItem.Text = "Shape Detection";
+            this.shapeDetectionToolStripMenuItem.Click += new System.EventHandler(this.shapeDetectionToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 391;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(391, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(405, 426);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(405, 426);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Shape Detection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shapeDetectionToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ 26_Shape_Detection_suin_Contours/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace _26_Shape_Detection_using_Contours
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> imgInput;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog()==DialogResult.OK)
+                {
+                    imgInput = new Image<Bgr, byte>(ofd.FileName);
+                    pictureBox1.Image = imgInput.Bitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void shapeDetectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (imgInput == null)
+                {
+                    throw new Exception("Please select an image");
+                }
+
+                var tmp = imgInput.SmoothGaussian(5).Convert<Gray, byte>()
+                    .ThresholdBinaryInv(new Gray(230), new Gray(255)); // Inv because the background of the image is white, so remove it
+
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat hier = new Mat();
+
+                CvInvoke.FindContours(tmp, contours,
+                    hier,
+                    Emgu.CV.CvEnum.RetrType.External,
+                    Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    double parameter = CvInvoke.ArcLength(contours[i], true);
+                    VectorOfPoint approx = new VectorOfPoint();
+
+                    CvInvoke.ApproxPolyDP(contours[i], approx, .04 * parameter, true);
+
+                    CvInvoke.DrawContours(imgInput, contours, i, new MCvScalar(255, 0, 255), 2);
+
+                    // Moments : concept for finding shaeps, area, measures...
+                    // Here we want the center of the shape
+                    var moments = CvInvoke.Moments(contours[i]);
+                    int x = (int) (moments.M10 / moments.M00);
+                    int y = (int) (moments.M01 / moments.M00);
+
+                    if (approx.Size == 3) // Triangle
+                    {
+                        CvInvoke.PutText(imgInput, "Triangle", 
+                            new Point(x, y), 
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex, 
+                            .5,
+                            new MCvScalar(255, 0, 0), 
+                            2);
+                    }
+
+                    if (approx.Size == 4) // Rectangle
+                    {
+                        // Check for Square
+                        Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+                        double ar = (double) rect.Width / rect.Height;
+                        if (ar >= .95 || ar <= 1.05)
+                        {
+                            CvInvoke.PutText(imgInput, "Square",
+                            new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex,
+                            .5,
+                            new MCvScalar(255, 0, 0),
+                            2);
+                        }
+                        else
+                        {
+                            CvInvoke.PutText(imgInput, "Rectangle",
+                            new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex,
+                            .5,
+                            new MCvScalar(255, 0, 0),
+                            2);
+                        }
+                    }
+
+                    if (approx.Size == 5) // Pentagon
+                    {
+                        CvInvoke.PutText(imgInput, "Pentagon",
+                            new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex,
+                            .5,
+                            new MCvScalar(255, 0, 0),
+                            2);
+                    }
+
+                    if (approx.Size == 6) // Hexagon
+                    {
+                        CvInvoke.PutText(imgInput, "Hexagon",
+                            new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex,
+                            .5,
+                            new MCvScalar(255, 0, 0),
+                            2);
+                    }
+
+                    if (approx.Size > 6) // Circle
+                    {
+                        CvInvoke.PutText(imgInput, "Circle",
+                            new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex,
+                            .5,
+                            new MCvScalar(255, 0, 0),
+                            2);
+                    }
+
+                    pictureBox2.Image = imgInput.Bitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
